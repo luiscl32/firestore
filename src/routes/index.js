10954c7faf0924c276299854383cf4ccc8fa6a80
luis correa <luiscl32@gmail.com>
2019-12-06 18:53:02 -0500

    added add function

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,41 @@
 const { Router } = require('express')
 const router = Router();
+const admin = require('firebase-admin')
+
+admin.initializeApp({
+  credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+})
+
+
+const db = admin.firestore()
+
 
 //routes
-router.get('/',(req, res) => {
-  res.render('index.hbs')
+
+
+//get
+
+
+
+//add
+router.post('/add-contact', (req, res) => {
+  const { body } = req
+  const  newContact = {
+    Firstname: body.Firstname,
+    Lastname: body.Lastname,
+    Email: body.Email,
+    Phone: bidy.Phone,
+  }
+
+  db.collection('contacts').add(...newContact)
+  .then(() => {
+    res.send({ status: 200, message: 'contact added' })
+  })
+  .catch((err) => {
+    res.send({ status: 500, message: err })
+  })
 })
+
 
 
 module.exports = router
